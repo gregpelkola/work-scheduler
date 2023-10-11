77d3ef4b898fe051735c7df58edb1699d2ec4ddc
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-10-11 18:47:36 -0500

    added call for loadData

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+loadData()
+
 $(function () {
 
   var currentDay = dayjs().format('dddd, MM D ');
